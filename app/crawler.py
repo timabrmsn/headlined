02a5f507632f12737d2f4291cdda6cf5959a8995
Tim Abramson <tim@abramson.dev>
2020-10-11 04:04:35 +0000

    updates

--- app/crawler.py
+++ app/crawler.py
@@ -14,20 +14,12 @@ from pathlib import Path
 from time import mktime, sleep
 from pytz import timezone
 
-from models import Headlines
+from models import db, Base, Headlines
 from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
-
-DATABASE_URL = os.environ["DATABASE_URL"]
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-db = SessionLocal()
 
 
 def fetch_rss_entries(file="urls.json"):
@@ -36,31 +28,12 @@ def fetch_rss_entries(file="urls.json"):
     for site in feeds:
         page = feedparser.parse(site["url"])
         if page.status < 400:
-            tld = tldextract.extract(page.href).registered_domain
-            source = page.href
             for entry in page.entries:
-                yield (entry, tld, source)
-
-
-def process_entry(entry, tld, source):
-    h = Headlines()
-    h.rss = entry
-    h.rss_id = entry.id
-    h.source = source
-    h.domain = tld
-    try:
-        db.add(h)
-        db.flush()
-        db.commit()
-        print(f"added: {entry.id}, {source}")
-    except IntegrityError as e:
-        db.rollback()
-    except Exception as e:
-        print(f"exception: {type(e)}\nentry_id: {entry.id}, source: {source}")
-        db.rollback()
+                entry.source = page.href
+                yield entry
 
 
 if __name__ == "__main__":
     for entry in fetch_rss_entries():
-        process_entry(*entry)
+        h = Headlines(entry, db)
 
--- app/main.py
+++ app/main.py
@@ -13,15 +13,13 @@ from sqlalchemy.types import DateTime
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 
-from models import Headlines
-
-from copy import copy
+from models import Headlines, Authors, Tags, Entry, Base
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+Base.metadata.create_all(engine)
 
 def get_db():
     db = SessionLocal()
@@ -31,41 +29,14 @@ def get_db():
         db.close()
 
 
-class Entry(BaseModel):
-    id: int
-    title: str
-    link: str
-    icon: str
-    pub: str
-    published: str
-    published_parsed: datetime
-    summary: Optional[str] = None
-    tags: Optional[List[str]] = []
-    author: List[str]
-    source: str
-
-    class Config:
-        orm_mode = True
-
 
 def _get_latest(db: Session):
     updated = []
     entries = (
-        db.query(Headlines)
-        .filter(
-            Headlines.rss["published"]
-            .astext
-            .cast(DateTime(timezone=True)) >= datetime.utcnow() - timedelta(days=10)
-            )
+        db.query(Headlines, Authors, Tags)
+        .filter(Headlines.published_parsed >= datetime.utcnow() - timedelta(days=10))
         .limit(25).all()
     )
-    for entry in entries:
-        e = Entry(**entry.rss)
-        e.pub = entry.domain
-        e.source = entry.source
-        e.icon = f"https://www.google.com/s2/favicons?sz=128&domain={entry.domain}"
-        updated.append(e)
-    return updated
 
 
 class cache:
